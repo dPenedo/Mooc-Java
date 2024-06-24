caedf85d255097823e577912e007fd64c141d545
Dpenedo <dpenedo@tutanota.com>
2024-06-24 17:52:45 -0300

    Part 13 - 5 completed

--- mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,37 @@
 package textstatistics;
 
 
-public class TextStatisticsApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class TextStatisticsApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) {
+        BorderPane layout = new BorderPane();
+
+        TextArea textArea = new TextArea();
+        HBox texts = new HBox();
+        texts.setSpacing(10);
+        texts.getChildren().add(new Label("Letters: 0"));
+        texts.getChildren().add(new Label("Words: 0"));
+        texts.getChildren().add(new Label("The longest word is:"));
+
+        layout.setBottom(texts);
+        layout.setCenter(textArea);
+        Scene scene = new Scene(layout);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
