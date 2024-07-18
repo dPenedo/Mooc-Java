fb36e263046e7541d473b364540152d696af45d4
daniel <dpenedo@tutanota.com>
2024-07-18 19:38:18 -0300

    Asteroids until staying within the window

--- mooc-java-programming-ii/part14-Part14_09.Asteroids/src/main/java/asteroids/AsteroidsApplication.java
+++ mooc-java-programming-ii/part14-Part14_09.Asteroids/src/main/java/asteroids/AsteroidsApplication.java
@@ -1,14 +1,89 @@
 package asteroids;
 
-public class AsteroidsApplication {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
+public class AsteroidsApplication extends Application {
 
     public static int partsCompleted() {
         // State how many parts you have completed using the return value of this method
         return 0;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        pane.setPrefSize(600, 400);
+
+        Ship ship = new Ship(150, 100);
+
+        List<Asteroid> asteroids = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Random random = new Random();
+            Asteroid asteroid = new Asteroid(random.nextInt(100), random.nextInt(100));
+            asteroids.add(asteroid);
+
+        }
+
+        pane.getChildren().add(ship.getCharacter());
+        asteroids.forEach(asteroid -> pane.getChildren().add(asteroid.getCharacter()));
+
+        Scene scene = new Scene(pane);
+
+        Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
+        scene.setOnKeyPressed((event) -> {
+            pressedKeys.put(event.getCode(), Boolean.TRUE);
+        });
+        scene.setOnKeyReleased((event) -> {
+            pressedKeys.put(event.getCode(), Boolean.FALSE);
+        });
+
+        Point2D movement = new Point2D(1, 0);
+
+        new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+
+                if (pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
+                    ship.turnLeft();
+                }
+                if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
+                    ship.turnRight();
+                }
+                if (pressedKeys.getOrDefault(KeyCode.UP, false)) {
+                    ship.acceleate();
+                }
+                ship.move();
+                asteroids.forEach(asteroid -> asteroid.move());
+                asteroids.forEach(asteroid -> {
+                    if (ship.collide(asteroid)) {
+                        stop();
+                    }
+                });
+            }
+        }.start();
+
+        stage.setTitle("Asteroids!");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
 }
